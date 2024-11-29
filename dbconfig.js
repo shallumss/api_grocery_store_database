cf37d8087ab8130e1455bf06f4975222639560fa
maguslord <fa22-bce-008@cuilahore.edu.pk>
2024-11-29 20:00:16 +0500

    added a .env file and made changes according to it , for security

--- dbconfig.js
+++ dbconfig.js
@@ -1,16 +1,19 @@
+require('dotenv').config();  // Read .env file if present
+
 const config = {    
-    user: 'test',
-    password: 'Rfvg@qwerty',
-    server: 'shallum.database.windows.net', // Azure SQL Server address
-    database: 'IMDB_Top_1000', // Database name you created in Azure
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER , // Azure SQL Server address
+    database:  process.env.DB_DATABASE, // Database name you created in Azure
 
     options: {
         trustedConnection: false,  // Azure doesn't support trusted connection
         encrypt: true,             // Required for Azure SQL Database
         enableArithAbort: true,
+        trustServerCertificate: true // Required for Azure SQL Database
     },
 
-    port: 1433,  // Default port for Azure SQL Database
+   
 };
 
 module.exports = config;
