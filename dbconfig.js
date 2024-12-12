3c7d89c14700463c63bf82b2993d5fe47fb90060
maguslord <fa22-bce-008@cuilahore.edu.pk>
2024-12-12 18:09:28 +0500

    added debuggig code

--- dbconfig.js
+++ dbconfig.js
@@ -1,10 +1,10 @@
 require('dotenv').config();  // Read .env file if present
 
 const config = {    
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    server: process.env.DB_SERVER , // Azure SQL Server address
-    database:  process.env.DB_DATABASE, // Database name you created in Azure
+    user: 'shallum',
+    password: 'Rfvg@qwerty123456??',
+    server: 'shallum.database.windows.net' , // Azure SQL Server address
+    database:  'store', // Database name you created in Azure
 
     options: {
         trustedConnection: false,  // Azure doesn't support trusted connection
@@ -16,8 +16,8 @@ const config = {
     authentication: {
         type: 'default',
         options: {
-            userName: process.env.DB_USER,
-            password: process.env.DB_PASSWORD
+            userName: 'shallum',
+            password: 'Rfvg@qwerty123456??'
         }
     },
     port: 1433 // Standard Azure SQL Database port
