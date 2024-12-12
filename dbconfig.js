b402259d957336e7db1470f4534b02112c3023d6
maguslord <fa22-bce-008@cuilahore.edu.pk>
2024-12-12 16:34:32 +0500

    temp changes

--- dbconfig.js
+++ dbconfig.js
@@ -1,10 +1,10 @@
 require('dotenv').config();  // Read .env file if present
 
 const config = {    
-    user: process.env.SQLAZURECONNSTR_DB_USER,
-    password: process.env.SQLAZURECONNSTR_DB_PASSWORD,
-    server: process.env.SQLAZURECONNSTR_DB_SERVER , // Azure SQL Server address
-    database:  process.env.SQLAZURECONNSTR_DB_DATABASE, // Database name you created in Azure
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER , // Azure SQL Server address
+    database:  process.env.DB_DATABASE, // Database name you created in Azure
 
     options: {
         trustedConnection: false,  // Azure doesn't support trusted connection
